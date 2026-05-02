a2dd226ff2af7a0275b7060554f277584c088085
somayya <somayya.adusumalli98@gmail.com>
2026-05-02 14:27:52 +0530

    shell-roboshop

--- shipping.sh
+++ shipping.sh
@@ -9,9 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.somayya.fun
+MONGODB_HOST=mongodb.somayya.store
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=mysql.somayya.fun
+MYSQL_HOST=mysql.somayya.store
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
